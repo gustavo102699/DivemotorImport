730ce69d06d01628004978fc8c187ef588e74876
Marice1314 <55213640+Marice1314@users.noreply.github.com>
2019-11-18 18:05:08 -0500

    Repository modificado

---
+++ IProductRepository.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.entity.Product;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Long> {
+	@Query("select count(l.productName) from Product l where l.productName=:productName")
+	public int buscarNombre(@Param("productName") String nombre);
+
+	@Query("from Product p where p.productName like %:productName%")
+	public List<Product> buscarProducto(@Param("productName") String nombreProducto);
+
+	@Query("select p from Product p where p.category.categoryName like %?1%")
+	public List<Product> findProductByNameCategory(String nombreCategoria);
+}
