4be68de54f99473a6fb3874c5a4e4c4ba90f2c2f
Marice1314 <55213640+Marice1314@users.noreply.github.com>
2019-11-19 10:17:47 -0500

    supplier repository actualizado

--- ISupplierRepository.java
+++ ISupplierRepository.java
@@ -16,4 +16,7 @@ public interface ISupplierRepository extends JpaRepository<Supplier, Long>{
 
 	@Query("from Supplier p where p.supplierName like %:supplierName%")
 	List<Supplier> buscarNombre(@Param("supplierName") String nombreSupplier);
+	
+	@Query(value = "select  b.supplier_name,count(pr.id_producto) from public.importation i join import_details ide on ide.id_importation = i.id_importation join product pr on ide.id_product = pr.id_producto join supplier b on b.id_supplier = pr.id_supplier group by b.supplier_name  ORDER BY COUNT(b.supplier_name) DESC limit 1", nativeQuery = true)
+	public List<String[]> suppliertop();
 }
